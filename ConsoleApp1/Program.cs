a338f2d061560dc26aa1ad6815a104b32bcd6d03
Евгений <burkov-evgenij@inbox.ru>
2026-03-06 13:52:27 +0500

    feat: ну теперь норм

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Programm
+{
+    static void Main()
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        int sum = num1 + num2;
+        Console.WriteLine($"Сумма чисел: {sum}");
+    }
+}
