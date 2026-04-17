835071506abfc51148b987618f4ece26a70c03af
Евгений <burkov-evgenij@inbox.ru>
2026-04-17 14:27:06 +0500

    fresh: RIght

---
+++ course-1/practices/39.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+public interface IPlayable
+{
+    void Play();
+}
+public class Piano : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Пианино играет мелодию");
+    }
+}
+
+public class Guitar : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Гитара играет аккорды");
+    }
+}
+
+public class Drum : IPlayable
+{
+    public void Play()
+    {
+        Console.WriteLine("Барабан отбивает ритм");
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        IPlayable[] instruments =
+        {
+    new Guitar(),
+    new Piano(),
+    new Drum()
+};
+
+        foreach (var i in instruments)
+            i.Play();
+    }
+}
---
+++ course-1/practices/40.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+public interface IReadable
+{
+    void Read(string filename);
+}
+
+public interface IWritable
+{
+    void Write(string filename, string content);
+}
+
+public interface ISavable
+{
+    void Save();
+}
+public class TextDocument : IReadable, IWritable, ISavable
+{
+    private string content;
+
+    public void Read(string filename)
+    {
+        Console.WriteLine($"Чтение из файла {filename}");
+        content = "Текст из файла";
+    }
+
+    public void Write(string filename, string content)
+    {
+        Console.WriteLine($"Запись в файл {filename}: {content}");
+        this.content = content;
+    }
+
+    public void Save()
+    {
+        Console.WriteLine($"Файл сохранён. Содержимое: {content}");
+    }
+}
+
+
+public class Program
+{
+    static void Main()
+    {
+        TextDocument doc = new TextDocument();
+        doc.Read("data.txt");
+        doc.Write("data.txt", "Привет, мир!");
+        doc.Save();
+    }
+}
